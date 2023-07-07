bf20ff595e28ec97e2b500a9c215d833d57b821f
Sanatptl <sanatpatel96@gmail.com>
2023-07-07 15:39:35 +0530

    Enable all CORS requests

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "https://blogmanagement-sanat.netlify.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use("/api/v1/posts", postRoute);
 
 //express global-error-handler middleware
